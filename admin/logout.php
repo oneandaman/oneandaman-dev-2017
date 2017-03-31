40d11e6fc316855a24f29d6214909bb845e0d7f8
Pong <Pong@Pong-Ultrabook>
2017-03-31 21:17:33 +0700

    logout

---
+++ admin/logout.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Project    : One Andaman
+Created on : 31/03/2017
+Author     : Nattapong  Kothong
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
